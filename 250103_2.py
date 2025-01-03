2e36367f9a8aa5b4e5fb58b5f6f32c1e69cb4543
chan0222 <gksmftor8745@naver.com>
2025-01-03 15:36:40 +0900

    Dos : 중복조합 추가 및 설명 예시

--- 250103_2.py
+++ 250103_2.py
@@ -27,3 +27,14 @@ print(result2)
 # arr2에서 2개의 원소를 뽑아 중복을 허용해 모든 가능한 조합을 생성합니다.
 # 중복을 허용하기 때문에, 같은 요소가 반복해 나올 수 있습니다.
 # repeat=2는 두 번 선택하는 것을 의미합니다.
+
+# 중복 조합 : 서로 다른 n개에서 중복을 포함해 순서를 고려하지 않고 r개를 뽑는 것을 의미  
+# 중복 조합 예시 
+# 딥러닝에서 학습된 모델의 결과를 활용하여 최종적인 결과를 생성하는 앙상블 방법이 존재 
+
+from itertools import combinations_with_replacement
+
+arr3 = ['a','b','c']
+# 원소 중에서 2개를 뽑는 모든 중복 조합 계산
+result3 = list(combinations_with_replacement(arr3,2))
+print(result3)
