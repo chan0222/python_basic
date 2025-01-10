32bf6d449361fcf08638a9ba310bf96f31ad00c0
chan0222 <gksmftor8745@naver.com>
2025-01-10 14:52:37 +0900

    feat: 체스판 칠하기

--- 1week_5day.py
+++ 1week_5day.py
@@ -1,10 +1,29 @@
-def count_change(board,x,y,start_color):
-    changes = 0
-    for i in range(8):
-        for j in range(8):
-            expect_color = 'w' if (i +j) % 2 == 0 else "B"
-            if start_color == 'B':
-                expect_color = 'B' if (i + j) % 2 == 2 else "W"
-            if board[x + i][y+j] != expect_color:
-                changes += 1
-    return changes    
+n,m = map(int,input().split())
+board = []
+result = []
+
+for _ in range(n):
+    board.append(input())
+
+for i in range(n-7):
+    for j in range(m-7):
+        draw1 = 0
+        draw2 = 0
+
+        for a in range(i,i+8):
+            for b in range(j,j+8):
+                if(a+b) % 2 == 0:
+                    if board[a][b] != 'B':
+                        draw1 += 1
+                    if board[a][b] != 'W':
+                        draw2 += 1
+                else:
+                    if board[a][b] != 'W':
+                        draw1 += 1
+                    if board[a][b] != 'W':
+                        draw2 += 1
+
+        result.append(draw1)
+        result.append(draw2)
+
+print(min(result))
