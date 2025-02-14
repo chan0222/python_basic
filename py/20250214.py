695bf14275a3d3eb0961e2ef63d4e1e9f9ce1192
chan0222 <gksmftor8745@naver.com>
2025-02-14 16:31:38 +0900

    init: 파이썬 자료구조 실습

---
+++ py/20250214.py
@@ -0,0 +1,12 @@
+# my_list = [10,20,30]
+# print(my_list)
+
+my_list =[30,40,50]
+my_list.append(10)
+my_list.append(15)
+my_list.append(20)
+
+print(my_list)
+
+sliced = my_list[3:] # 3번째 자리부터 끝까지 
+print("sliced:",sliced)
