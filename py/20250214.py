b1ccaf84ee7ce2f8efeabb7aceb6b5cc88078f6e
chan0222 <gksmftor8745@naver.com>
2025-02-17 10:45:46 +0900

    feat : 자료구조 연산 3

--- py/20250214.py
+++ py/20250214.py
@@ -25,3 +25,41 @@
 
 # 만약 리스트에 있는 값이 아니라 없는 값을 물어본다먼? 에러발생
 
+# #리스트의 정렬
+# numbers = [2,4,8,7,3,5,1,6]
+# print("unsorted", numbers) #순서가 중요함
+
+# numbers.sort()
+# print("sorted", numbers)
+
+# numbers.sort(reverse= True)
+# print("sorted reverse", numbers)
+
+# 리스트 연산 요소 추가 및 삭제 
+list = []
+list.append(10)
+list.append(11)
+list.append(12)
+# print(list)
+
+list.extend([13,14,15]) # 하나의 리스트의 요소로 들어감
+# list.append([16,17]) # 리스트 형태로 요소로 들어감 // 복합 리스트의 형태로 들어감 넣은 값이 하나의 요소로
+# print(list)
+
+# print(list[-1]) # 리스트의 마지막 요소를 
+
+# 리스트 연산
+# new_list = list + [0,1,2]
+# print(new_list)
+
+# multi_list = [0,1,2] * 3
+# print(multi_list)
+
+# print(list)
+# del list[0] # del 리스트 삭제하고자 하는 요소 자리
+# print(list)
+
+# 리스트를 활용한 최대값& 최소값 찾아오는 것
+max_value = max(list)
+min_value = min(list)
+print(f"최대 값은 {max_value}, 최고 값은 {min_value} 입니다.")
