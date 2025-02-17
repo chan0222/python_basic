269c95f0f7b028606804d1a4d950d1a0e5c50dbd
chan0222 <gksmftor8745@naver.com>
2025-02-17 10:01:25 +0900

    feat : 리스트 요소 찾기

--- py/20250214.py
+++ py/20250214.py
@@ -1,12 +1,27 @@
 # my_list = [10,20,30]
 # print(my_list)
 
-my_list =[30,40,50]
-my_list.append(10)
-my_list.append(15)
-my_list.append(20)
+# my_list =[30,40,50]
+# my_list.append(10)
+# my_list.append(15)
+# my_list.append(20)
 
-print(my_list)
+# print(my_list)
 
-sliced = my_list[3:] # 3번째 자리부터 끝까지 
-print("sliced:",sliced)
+# sliced = my_list[3:] # 3번째 자리부터 끝까지 
+# print("sliced:",sliced)
+
+# 검색하기 좋은 리스트
+
+# fruits = ["banana","apple","blueberry","cherry"]
+
+# 바나나가 포함 되어있나요?
+# is_banana_included = "banana" in fruits
+# print("is banana included?", is_banana_included)
+
+# 체리는 어디에 있나요?
+# index_cherry =  fruits.index("cherry")
+# print("cherry is ", index_cherry)
+
+# 만약 리스트에 있는 값이 아니라 없는 값을 물어본다먼? 에러발생
+
