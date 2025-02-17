e798ae4de89a0a2ef43f31d52e0a2f6ff5449026
chan0222 <gksmftor8745@naver.com>
2025-02-17 15:20:13 +0900

    init :  자료구조 활용 실습 기초

---
+++ py/20250217_4.py
@@ -0,0 +1,21 @@
+while True:
+    print("")
+    print("할 일 목록 관리자")
+    print("1.. 할 일 추가")
+    print("2. 할 일 삭제")
+    print("3. 할 일 목록 보기")
+    print("4. 종료")
+
+
+    choice = input("선택:")
+
+    if choice == "1":
+        pass
+    elif choice == "2":
+        pass
+    elif choice == "3":
+        pass
+    elif choice == "4":
+        break
+    else:
+        print("올바른 선택이 아닙니다. 다시 시도해보세요")
